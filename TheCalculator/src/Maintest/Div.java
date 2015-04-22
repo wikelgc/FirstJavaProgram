c729ff718698a729cba7e598d1636259aaf4e525
wikelgc <1245788423@qq.com>
2015-04-22 22:26:54 +0800

    111
    111

---
+++ TheCalculator/src/Maintest/Div.java
@@ -0,0 +1,18 @@
+package Maintest;
+
+public class Div {
+//	int a;
+//	int b;
+//	int c;
+	
+	public static int division(int a){
+		return a;
+	}
+	public static int division(int a,int b){
+		return a+b;
+	}
+	public static int division(int a,int b,int c){
+		return a+b+c;
+	}
+
+}
